53c829b709e8ae5d2947ce9ac3d98a9e78a25bbd
Alejandro Novellino <alnovellino07@gmail.com>
2025-03-12 19:59:32 +0000

    feat add first exercise

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
+print(df.head())
+print()
 
 # 2
-df['overweight'] = None
+bmi = df['weight'] / ((df['height'] / 100) ** 2)
+df['overweight'] = bmi.map(lambda x: 1 if x > 25 else 0)
 
 # 3
+# normalize cholesterol
+df['cholesterol'] = df['cholesterol'].map(lambda x: 0 if x <= 1 else 1)
 
+# normalize glucose
+df['gluc'] = df['gluc'].map(lambda x: 0 if x <= 1 else 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    """
+    Create a DataFrame for the cat plot using pd.melt with values from cholesterol, gluc, smoke, alco, active, and overweight in the df_cat variable.
+    """
+    df_cat = pd.melt(df, id_vars=['id', 'cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
+    print(df_cat)
+    print()
 
     # 6
-    df_cat = None
+    """
+    Group and reformat the data in df_cat to split it by cardio. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+    """
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='count')
+
+    print(df_cat)
+    print()
     
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
+    # 7 and 8
+    fig = sns.catplot(data=df_cat, x='variable', y='count', hue='value', col='cardio', kind="bar")
 
     # 9
     fig.savefig('catplot.png')
@@ -58,3 +70,6 @@ def draw_heat_map():
     # 16
     fig.savefig('heatmap.png')
     return fig
+
+
+draw_cat_plot()
